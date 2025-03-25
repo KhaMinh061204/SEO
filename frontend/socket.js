be3a4b0ade682b415323b2c2b5c35d867dd6ec49
KhaMinh <minhkha06122004@gmail.com>
2025-03-25 09:27:05 +0700

    update

--- frontend/socket.js
+++ frontend/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io("https://ceecine.onrender.com/"); // URL backend có socket.io
+const socket = io("https://ceecine.onrender.com/"); // URL backend socket.io 
 
 export default socket;
