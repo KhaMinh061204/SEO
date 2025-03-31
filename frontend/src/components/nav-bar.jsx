e7daf48653206472e59a16b8fa1fc5aff5bac466
nhpl894 <22520815@gm.uit.edu.vn>
2025-03-31 23:11:44 +0700

    modify nav-bar

--- frontend/src/components/nav-bar.jsx
+++ frontend/src/components/nav-bar.jsx
@@ -1,20 +1,20 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react-hooks/rules-of-hooks */
-import { useLocation, useNavigate } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import $ from 'jquery'
-import logo from '../assets/img/logo.png'
-import DropDownMenu from '../components/drop-down-menu'
-import SearchBar from './SearchBar'
-import { get } from '../api/api'
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import $ from 'jquery';
+import { Menu } from 'lucide-react';
+import logo from '../assets/img/logo.png';
+import DropDownMenu from '../components/drop-down-menu';
+import SearchBar from './SearchBar';
+import { get } from '../api/api';
 
-function navbar() {
-    const navigate = useNavigate()
+function Navbar() {
+    const navigate = useNavigate();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    useEffect(() => { }, [location])
+    useEffect(() => { }, [location]);
     const locat = useLocation();
-    const token = localStorage.getItem('token')
-    const expires = sessionStorage.getItem('expires')
+    const token = localStorage.getItem('token');
+    const expires = sessionStorage.getItem('expires');
     const [user, setUser] = useState({ avatar: '' });
 
     const nav_config = () => {
@@ -24,50 +24,27 @@ function navbar() {
         }
         const path = locat.pathname.split("/")[1];
         $("#" + path).addClass("nav_link_active");
-    }
+    };
 
     const handleSearch = searchTerm => {
-        // Chuyển hướng đến trang tìm kiếm với từ khóa tìm kiếm trong URL
         if (searchTerm) {
-            navigate(`/search?title=${searchTerm}`)
+            navigate(`/search?title=${searchTerm}`);
         }
-    }
+    };
 
-    // useEffect(() => {
-    //     if (token && expires) {
-    //         const currentTime = Date.now()
-    //         if (currentTime < expires) {
-    //             $('#login').addClass('hide')
-    //             $('#img_account_top').removeClass('hide')
-    //         } else {
-    //             localStorage.removeItem('token')
-    //             sessionStorage.removeItem('token')
-    //             sessionStorage.removeItem('expires')
-    //             $('#login').removeClass('hide')
-    //             $('#img_account_top').addClass('hide')
-    //         }
-    //     } else {
-    //         $('#login').removeClass('hide')
-    //         $('#img_account_top').addClass('hide')
-    //     }
-    //     nav_config();
-
-    // }, [token, expires, locat.pathname])
     useEffect(() => {
         const fetchProfile = async () => {
             try {
-                // Chỉ gọi API nếu token tồn tại và hợp lệ
                 if (token && expires) {
                     const currentTime = Date.now();
                     if (currentTime < expires) {
                         const profileData = await get('/account/profile');
                         setUser({
-                            avatar: profileData.avatar, // Lấy avatar từ API
+                            avatar: profileData.avatar,
                         });
                         $('#login').addClass('hide');
                         $('#img_account_top').removeClass('hide');
                     } else {
-                        // Token đã hết hạn
                         localStorage.removeItem('token');
                         sessionStorage.removeItem('token');
                         sessionStorage.removeItem('expires');
@@ -75,7 +52,6 @@ function navbar() {
                         $('#img_account_top').addClass('hide');
                     }
                 } else {
-                    // Không có token
                     $('#login').removeClass('hide');
                     $('#img_account_top').addClass('hide');
                 }
@@ -91,93 +67,103 @@ function navbar() {
     }, [token, expires, locat.pathname]);
 
     return (
-        <>
-            {/* nav side */}
-
-            <header>
-                <div className='wrap_header_nav'>
-                    <div className='wrap_right_nav'>
-                        <div
-                            onClick={() => {
-                                navigate('/')
-                                window.location.reload()
-                            }}
-                        >
-                            <img id='logo-trans' src={logo} width={150} />
-                        </div>
-                    </div>
-
-                    <div className='wrap_center_ul'>
-                        <ul className='center_ul' id='nav-center'>
-                            <li
-                                className='nav_link nav_header'
-                                id='home'
-                                onClick={() => {
-                                    navigate('/')
-                                }}
-                            >
-                                Trang chủ
-                            </li>
-                            <li
-                                className='nav_link nav_header'
-                                id='filmlist'
-                                onClick={() => {
-                                    navigate('/filmlist')
-                                }}
-                            >
-                                Phim
-                            </li>
-                            <li
-                                className='nav_link nav_header'
-                                id='cinemalist'
-                                onClick={() => {
-                                    navigate('/cinemalist')
-                                }}
-                            >
-                                Rạp chiếu
-                            </li>
-                            <li
-                                className='nav_link nav_header'
-                                id='promotion'
-                                onClick={() => {
-                                    navigate('/promotion')
-                                }}
-                            >
-                                Khuyến mãi
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className='wrap_auth_btn' id='auth_btn'>
-                        <SearchBar onSearch={handleSearch} />
-                        <button
-                            onClick={() => {
-                                $('#authpopup').removeClass('hide')
-                            }}
-                            className='btn nav_header'
-                            id='login'
-                        >
-                            <p>Đăng nhập</p>
-                        </button>
-                        <div
-                            id='img_account_top'
-                            className='wrap_img_account_top hide'
-                            style={{ position: 'relative' }}
-                        >
-                            {/* onClick={()=>{navigate("/profile/info")}} */}
-                            <img
-                                src={user.avatar}
-                                alt='User Avatar'
-                                className='userimg-prod'
-                                style={{ width: '35px', height: '35px', marginRight: '5px' }}
-                            />
-                            <DropDownMenu />
-                        </div>
+        <header className="header">
+            <div className="wrap_header_nav">
+                <div className="wrap_right_nav">
+                    <div onClick={() => {
+                        navigate('/');
+                        window.location.reload();
+                    }}>
+                        <img id="logo-trans" src={logo} width={150} alt="Logo" />
                     </div>
                 </div>
-            </header>
-        </>
-    )
+
+                {/* Desktop Menu */}
+                <div className="wrap_center_ul hidden md:block">
+                    <ul className="center_ul" id="nav-center">
+                        <li className="nav_link nav_header" id="home" onClick={() => navigate('/')}>
+                            Trang chủ
+                        </li>
+                        <li className="nav_link nav_header" id="filmlist" onClick={() => navigate('/filmlist')}>
+                            Phim
+                        </li>
+                        <li className="nav_link nav_header" id="cinemalist" onClick={() => navigate('/cinemalist')}>
+                            Rạp chiếu
+                        </li>
+                        <li className="nav_link nav_header" id="promotion" onClick={() => navigate('/promotion')}>
+                            Khuyến mãi
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="wrap_auth_btn" id="auth_btn">
+                    <div className="hidden md:block">
+                        <SearchBar onSearch={handleSearch} />
+                    </div>
+                    <button
+                        onClick={() => $('#authpopup').removeClass('hide')}
+                        className="btn nav_header hidden md:block"
+                        id="login"
+                    >
+                        <p>Đăng nhập</p>
+                    </button>
+                    <div
+                        id="img_account_top"
+                        className="wrap_img_account_top hide hidden md:block"
+                        style={{ position: 'relative' }}
+                    >
+                        <img
+                            src={user.avatar}
+                            alt="User Avatar"
+                            className="userimg-prod"
+                            style={{ width: '35px', height: '35px', marginRight: '5px' }}
+                        />
+                        <DropDownMenu />
+                    </div>
+
+                    {/* Mobile Menu Button */}
+                    <button
+                        className="mobile-menu-button md:hidden"
+                        onClick={() => setIsMenuOpen(!isMenuOpen)}
+                    >
+                        <Menu size={24} />
+                    </button>
+                </div>
+            </div>
+
+            {/* Mobile Menu */}
+            <div className={`mobile-menu ${isMenuOpen ? 'show' : ''}`}>
+                <ul className="mobile-menu-items">
+                    <li onClick={() => { navigate('/'); setIsMenuOpen(false); }}>
+                        Trang chủ
+                    </li>
+                    <li onClick={() => { navigate('/filmlist'); setIsMenuOpen(false); }}>
+                        Phim
+                    </li>
+                    <li onClick={() => { navigate('/cinemalist'); setIsMenuOpen(false); }}>
+                        Rạp chiếu
+                    </li>
+                    <li onClick={() => { navigate('/promotion'); setIsMenuOpen(false); }}>
+                        Khuyến mãi
+                    </li>
+                    {/* <li className="mobile-search">
+                        <SearchBar onSearch={handleSearch} />
+                    </li> */}
+                    <li>
+                        <button
+                            onClick={() => {
+                                $('#authpopup').removeClass('hide');
+                                setIsMenuOpen(false);
+                            }}
+                            className="mobile-login-btn"
+                        >
+                            Đăng nhập
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    );
 }
 
-export default navbar
+export default Navbar;
